26b643999766cd246e8a0f78b17c497100a0a875
Brian Nicoloff <brian.nicoloff@gmail.com>
2016-05-26 01:15:20 -0500

    solution - Started working through the excercise. - BN

--- app/scorecard.js
+++ app/scorecard.js
@@ -1,8 +1,37 @@
+'use strict';
+
 module.exports = function () {
+    var totalScore = 0;
+    var frames = {};
+    var currentFrame = 1;
+    var currentFrameBowl = 1;
 
-    this.users = [];
+    this.addScore = function (score) {
+        if (score > 10) {
+            throw 'Out of range exception';
+        }
+        
+        if (!frames[currentFrame]) {
+            frames[currentFrame] = {}
+        }
+        
+        frames[currentFrame][currentFrameBowl] = score;
+        
+        currentFrame = currentFrameBowl === 1 ? currentFrame : currentFrame + 1;
+        currentFrameBowl =  currentFrameBowl === 1 ? 2 : 1;
+        
+        totalScore += score;    
+    }
 
-    this.addUser = function (name) {
-        this.users.push({ name: name });
+    this.frameScore = function (frame) {
+        var frameScore = frames[frame][1] + frames[frame][2];
+        if (frameScore > 10) {
+            throw 'cannot bowl more than 10 in a frame';
+        }
+        return frameScore;
+    }
+
+    this.totalScore = function () {
+        return totalScore;
     }
 }
--- app/scorecard.tests.js
+++ app/scorecard.tests.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var scorecard = require('./scorecard'); 
 
 suite('scorecard', function () {
@@ -7,13 +9,55 @@ suite('scorecard', function () {
         testObject = new scorecard(); 
     });
     
-    test('should track user names', function () {                
-        testObject.addUser('Brian');
-        testObject.addUser('Joe');
+    test('add score adds to totalScore', function () {
+        testObject.addScore(4);
 
-        assert.equal(testObject.users.length, 2);        
-        assert.equal(testObject.users[0].name, 'Brian');
-        assert.equal(testObject.users[1].name, 'Joe');
+        var result = testObject.totalScore();
+        
+        assert.equal(result, 4);
+    });
+
+    test('add multiple scores adds to total score', function () {
+        testObject.addScore(1);
+        testObject.addScore(1);
+        testObject.addScore(1);
+
+        var result = testObject.totalScore();
+
+        assert.equal(result, 3);
+    });
+
+    [11, 20, 30].forEach(function (score) {
+        test('cannot add more than 10 at a time - score = ' + score, function () {
+            try {
+                testObject.addScore(score);
+                assert.fail('should have thrown out of range exception!');
+            } catch (ex) {
+                assert.equal(ex, 'Out of range exception');
+            }
+        });
+    });
+
+    test('adds two bowl scores per frame', function () {
+        testObject.addScore(1);
+        testObject.addScore(2);
+        testObject.addScore(3);
+        testObject.addScore(4);
+
+        assert.equal(testObject.frameScore(1), 3);
+        assert.equal(testObject.frameScore(2), 7);
+    }); 
+    
+    test('does not add more than 10 to a frame', function () {
+        testObject.addScore(8);
+        testObject.addScore(3);
+
+        try {
+            testObject.frameScore(1);
+            assert.fail('should not have gotten here');
+        } catch (ex){
+            assert.equal(ex, 'cannot bowl more than 10 in a frame');
+        }
     });
     
 });
